e1d1952dddca719c489ea93e87f280bb72ca613e
Mimmo <dm@arisanlab.it>
2025-03-17 10:31:41 +0100

    fix

--- html/app/Listeners/CarburanteAddOrUpdListener.php
+++ html/app/Listeners/CarburanteAddOrUpdListener.php
@@ -6,6 +6,7 @@ use App\Events\Illuminate\Events\CarburanteAddOrUpdEvent;
 use App\Models\Cisterna;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class CarburanteAddOrUpdListener
 {
@@ -23,6 +24,7 @@ class CarburanteAddOrUpdListener
     public function handle(CarburanteAddOrUpdEvent $event): void
     {
         $evt = $event->el;
+        Log::info('CarburanteAddOrUpdListener: ' . json_encode($evt));
 
         if ($evt['old_cisterne_id'] ) {
             Cisterna::where('id', $evt['old_cisterne_id'])->increment('livello_attuale', $evt['old_litri']);
@@ -33,15 +35,17 @@ class CarburanteAddOrUpdListener
             Cisterna::where('id', $evt['cisterne_id'])->decrement('livello_attuale', $evt['litri']);
         }
 
-        $checkLimit = Cisterna::where('id', $evt['cisterne_id'])->first();
-        if ($checkLimit->livello_attuale <= $checkLimit->livello_minimo) {
-            // parte avviso
-            $gruppi_ids = $checkLimit->gruppi_ids ? json_decode($checkLimit->gruppi_ids) : [];
-            if (count($gruppi_ids)) {
-				$emails = fromGruppiIdsToUserEmail($gruppi_ids);				
-                $message = 'Il livello della cisterna '.$checkLimit->label.' è al di sotto del minimo consentito';
-                $message .= '<br>Livello attuale: '.$checkLimit->livello_attuale;
-                sendEmailGenerica(null, $emails, 'Alert livello cisterna', $message);
+        if ($evt['cisterne_id']) {
+            $checkLimit = Cisterna::where('id', $evt['cisterne_id'])->first();
+            if ($checkLimit->livello_attuale <= $checkLimit->livello_minimo) {
+                // parte avviso
+                $gruppi_ids = $checkLimit->gruppi_ids ? json_decode($checkLimit->gruppi_ids) : [];
+                if (count($gruppi_ids)) {
+                    $emails = fromGruppiIdsToUserEmail($gruppi_ids);
+                    $message = 'Il livello della cisterna '.$checkLimit->label.' è al di sotto del minimo consentito';
+                    $message .= '<br>Livello attuale: '.$checkLimit->livello_attuale;
+                    sendEmailGenerica(null, $emails, 'Alert livello cisterna', $message);
+                }
             }
         }
 
